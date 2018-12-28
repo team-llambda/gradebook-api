585ee9b6dd320e1a11e289b75a5f2a7f88e50b78
Terrance Li <terranceli.terbear@gmail.com>
2018-12-27 17:41:13 -0800

    Initial functionality

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-const login = () => {
-	return new Promise((res, rej) => {})
+let apiKey
+const BASE_URL = 'https://grades.llambda.net'
+
+const setAPIKey = key => {
+	apiKey = key
+}
+
+const login = (username, password) => {
+	// if (!apiKey) return
+
+	return fetch(BASE_URL + '/api/v1/authenticate', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			username,
+			password
+		}),
+		credentials: 'include'
+	})
+}
+
+const logout = () => {
+	return fetch(BASE_URL + '/api/v1/logout', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		credentials: 'include'
+	})
+}
+
+const getClasses = () => {
+	return fetch(BASE_URL + '/api/v1/classes', {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		},
+		credentials: 'include'
+	})
+}
+
+const getClass = (period, quarter = 'current') => {
+	return fetch(BASE_URL + `/api/v1/class/${period}?quarter=${quarter}`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		},
+		credentials: 'include'
+	})
 }
 
 exports = {
-	login
+	login,
+	logout,
+	getClasses,
+	getClass
 }
